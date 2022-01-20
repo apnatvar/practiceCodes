a38bed1c5fd4153c8a04c2974550ff99066d8efd
apnatvar <95866059+apnatvar@users.noreply.github.com>
2022-01-20 16:51:12 +0000

    Update AlphaToASCII.py

--- AlphaToASCII.py
+++ AlphaToASCII.py
@@ -1,4 +1,7 @@
 a_string = input("Enter a string to display their relevant ASCII Values\n")
 indexNumber = []
 for character in a_string:
-   print(ord(character))
+   if (character == " "):#ignores all spaces in your text
+      continue
+   #incase you need to add spaces comment out the above 2 line
+   print("Original Character:",character,"- ASCII Value:",ord(character))
